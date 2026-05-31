b6d9d904b6b450e36c2d09fc6ffc23d02eca1626
Om Bhavsar <ombhavsar28feb2005@gmail.com>
2026-05-31 11:42:22 +0530

    The Setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    # this function will return the list of requirements
+    requirements = []
+    with open(file_path,'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+setup(
+    name='ML Project',
+    version='0.0.1',
+    author='Om Bhavsar',
+    email='ombhavsar28feb2005@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
